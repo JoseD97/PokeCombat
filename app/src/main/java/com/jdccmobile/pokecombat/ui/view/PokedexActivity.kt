91db811254c8f662b5b1110c2eddb5d33a9d8a29
Jose <josedcastro97@gmail.com>
2023-05-06 11:29:58 +0200

    fragment functionality implemented

--- app/src/main/java/com/jdccmobile/pokecombat/ui/view/PokedexActivity.kt
+++ app/src/main/java/com/jdccmobile/pokecombat/ui/view/PokedexActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.core.os.bundleOf
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.jdccmobile.pokecombat.R
@@ -30,27 +27,35 @@ class PokedexActivity @Inject constructor() : AppCompatActivity() {
         binding = ActivityPokedexBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initActivity()
         pokedexViewModel.initViewModel()
+    }
+
+    private fun initActivity() {
         initRecyclerView()
+        // TODO OCULTAR VISTA CUANDO SE VUELVE CON EL BACK ON CLICK LISTENER DESDE EL COMBAT
+        binding.frSelectedPokemonContainer.visibility = View.GONE
     }
 
     private fun initRecyclerView() {
+        // TODO AÑADIR CIRCULO DE CARGA MIENTRAS SE CARGAN LOS POKEMONS
         binding.rvPokemon.layoutManager = GridLayoutManager(this, 2)
-        pokedexViewModel.pokemonsList.observe(this, Observer { pokemon ->
-            binding.rvPokemon.adapter = PokedexAdapter(pokemon) })
+        pokedexViewModel.pokemonsList.observe(this) { pokemon ->
+            binding.rvPokemon.adapter = PokedexAdapter(pokemon) { pokemonId ->
+                onItemSelected(pokemonId)
+            }
+        }
     }
-//
-//    private fun onItemSelected(pokemonId: Int){ // todo abrir fragment al pulsar el boton // poner sobra fuera del cardview
-//        Toast.makeText(this, "$pokemonId", Toast.LENGTH_SHORT).show()
-//        val bundle = bundleOf(POKEMON_ID to pokemonId)
-//        val selectPokemonFragment = SelectPokemonFragment()
-//        selectPokemonFragment.arguments = bundle
-//        supportFragmentManager.beginTransaction()
-//            .add(R.id.frSelectPokemonContainer, selectPokemonFragment)
-//            .addToBackStack(null)
-//            .commit()
-//        binding.frSelectPokemonContainer.visibility = View.VISIBLE
-//    }
+
+    private fun onItemSelected(pokemonId: Int){ // todo abrir fragment al pulsar el boton // poner sobra fuera del cardview
+        Toast.makeText(this, "$pokemonId", Toast.LENGTH_SHORT).show()
+        val selectedPokemonFragment = SelectedPokemonFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.frSelectedPokemonContainer, selectedPokemonFragment)
+            .addToBackStack(null)
+            .commit()
+        binding.frSelectedPokemonContainer.visibility = View.VISIBLE
+    }
 
 
 
